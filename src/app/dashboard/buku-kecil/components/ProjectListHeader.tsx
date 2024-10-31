01b75196a9779de4bedd61cb89adeef5522c3b39
kdan0012 <127380884+kent-daniel@users.noreply.github.com>
2024-10-31 16:02:52 +0700

    make way for project list

--- src/app/dashboard/buku-kecil/components/ProjectListHeader.tsx
+++ src/app/dashboard/buku-kecil/components/ProjectListHeader.tsx
@@ -3,14 +3,20 @@
 import React from "react";
 import { NewProjectForm } from "./NewProjectForm";
 import { PopoverForm } from "./popover-form";
+import { Separator } from "@/components/ui/separator";
 
 const ProjectListHeader = () => {
   return (
     <>
-      <h1 className="text-4xl font-semibold">Projects</h1>
-      <PopoverForm name="Tambah project" title="Tambah Project Baru">
-        {(closePopover) => <NewProjectForm closeForm={() => closePopover()} />}
-      </PopoverForm>
+      <div className="flex justify-between p-3 mt-7">
+        <h1 className="text-4xl font-semibold">Projects</h1>
+        <PopoverForm name="Tambah project" title="Tambah Project Baru">
+          {(closePopover) => (
+            <NewProjectForm closeForm={() => closePopover()} />
+          )}
+        </PopoverForm>
+      </div>
+      <Separator className="bg-emerald-700" />
     </>
   );
 };
--- src/app/dashboard/buku-kecil/page.tsx
+++ src/app/dashboard/buku-kecil/page.tsx
@@ -1,18 +1,17 @@
-import { Separator } from "@/components/ui/separator";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ProjectListHeader from "./components/ProjectListHeader";
 import { BackToDashboardButton } from "../components/BackToDashboardButton";
+import { ProjectList } from "./components/ProjectList";
 
 const page = () => {
   return (
     <>
       <BackToDashboardButton />
       <div className="mx-auto w-1/2 flex flex-col justify-center">
-        <div className="flex justify-between p-3 mt-7">
-          <ProjectListHeader />
-        </div>
-        <Separator className="bg-emerald-800" />
+        <ProjectListHeader />
+
+        <ProjectList />
         <ToastContainer />
       </div>
     </>
