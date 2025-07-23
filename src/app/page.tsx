4e037a36ab4bcdae688271b66dcbf2a16780d30c
kdan0012 <127380884+kent-daniel@users.noreply.github.com>
2025-07-24 00:19:42 +1000

    remove import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import Image from "next/image";
-import {
-  RegisterLink,
-  LoginLink,
-} from "@kinde-oss/kinde-auth-nextjs/components";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
